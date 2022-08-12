848ae482eb35ece3e2d9ac5db8d867602e3f731f
Nail Muhamatnurov <nailmuhamatnurov@yandex.ru>
2022-08-12 09:41:29 +0500

    16

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 const User = require('../models/user');
 const NotFoundError = require('../errors/notFoundError');
-const { ERROR_CODE_BAD_REQUEST, ERROR_CODE_NOT_FOUND, ERROR_CODE_SERVER_ERROR } = require('../errors/errorsStatus');
 
 const getUsers = (req, res, next) => {
   User.find({})
@@ -15,70 +14,53 @@ const createUser = (req, res, next) => {
     .catch(next);
 };
 
-const getUserById = async (req, res) => {
-  try {
-    const findedUser = await User.findById(req.params.userId);
-    if (!findedUser) {
-      throw new NotFoundError('Пользователь не найден');
-    }
-    res.send({ data: findedUser });
-  } catch (err) {
-    if (err instanceof NotFoundError) {
-      res.status(ERROR_CODE_NOT_FOUND).send({ message: err.message });
-    } else if (err.name === 'CastError') {
-      res.status(ERROR_CODE_BAD_REQUEST).send({ message: 'Некоректные данные' });
-    } else {
-      res.status(ERROR_CODE_SERVER_ERROR).send({ message: 'Сервер не отвечает' });
-    }
-  }
+const getUserById = (req, res, next) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному _id не найден');
+      }
+      res.send({ data: user });
+    })
+    .catch(next);
 };
 
-const updateUser = async (req, res) => {
-  try {
-    const { name, about } = req.body;
-    const findedUser = await User.findByIdAndUpdate(
-      req.user._id,
-      { name, about },
-      { new: true, runValidators: true },
-    );
-    if (!findedUser) {
-      throw new NotFoundError('Пользователь не найден');
-    }
-    res.send({ data: findedUser });
-  } catch (err) {
-    if (err instanceof NotFoundError) {
-      res.status(ERROR_CODE_NOT_FOUND).send({ message: err.message });
-    }
-    if (err.name === 'ValidationError') {
-      res.status(ERROR_CODE_BAD_REQUEST).send({ message: 'Некоректные данные' });
-    }
-    if (err.name === 'ServerError') {
-      res.status(ERROR_CODE_SERVER_ERROR).send({ message: 'Сервер не отвечает' });
-    }
-  }
+const updateUser = (req, res, next) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному _id не найден');
+      }
+      res.send({ data: user });
+    })
+    .catch(next);
 };
 
-const updateAvatar = async (req, res) => {
-  try {
-    const { avatar } = req.body;
-    const findedAvatar = await User.findByIdAndUpdate(
-      req.user._id,
-      { avatar },
-      { new: true, runValidators: true },
-    );
-    if (!findedAvatar) {
-      throw new NotFoundError('Пользователь не найден');
-    }
-    res.send({ data: findedAvatar });
-  } catch (err) {
-    if (err instanceof NotFoundError) {
-      res.status(ERROR_CODE_NOT_FOUND).send({ message: err.message });
-    } else if (err.name === 'ValidationError') {
-      res.status(ERROR_CODE_BAD_REQUEST).send({ message: 'Некоректные данные' });
-    } else {
-      res.status(ERROR_CODE_SERVER_ERROR).send({ message: 'Сервер не отвечает' });
-    }
-  }
+const updateAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному _id не найден');
+      }
+      res.send({ data: user });
+    })
+    .catch(next);
 };
 
 module.exports = {
