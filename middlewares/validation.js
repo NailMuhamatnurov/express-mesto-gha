69600a1073de0b1ac82492682956450505953ade
Nail Muhamatnurov <nailmuhamatnurov@yandex.ru>
2022-08-24 15:50:53 +0500

    Проектная 14 ошибка валидации joi

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -31,7 +31,7 @@ const validateAuth = celebrate({
 const validateCreateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: checkedLink,
+    link: Joi.string().required(),
   }),
 });
 
