57e63671ab8cef80916bbd0c5d7dd7167d0af515
Nail Muhamatnurov <nailmuhamatnurov@yandex.ru>
2022-08-25 09:56:41 +0500

    Проектная 14

--- controllers/users.js
+++ controllers/users.js
@@ -16,22 +16,7 @@ const deleteEmptyField = (obj) => {
 
 const getUsers = (req, res, next) => {
   User.find({})
-    .then((users) => {
-      res.send(users.map((user) => {
-        const {
-          name,
-          about,
-          avatar,
-          _id,
-        } = user;
-        return {
-          _id,
-          name,
-          about,
-          avatar,
-        };
-      }));
-    })
+    .then((user) => res.send(user))
     .catch(next);
 };
 
